1b06e8416cbb33441a40999d99987c4fdbd1e532
Eliott Hendryx-Parker <eliott@sixfeetup.com>
2026-01-31 18:05:42 -0500

    Added parametric sweeping to identify the optimal time for the mission guidance.

--- guidance.py
+++ guidance.py
@@ -24,25 +24,59 @@ target_dr = 0  # m/s
 target_theta = theta0 - 480_000 / r_moon  # rad
 target_dtheta = 0  # rad/s
 
-T = 714
+T = 580
 t = np.linspace(0, T, 100)
 
-# --- Polynomial Guidance (r) ---
-a = np.array([[0, 0, 0, 1], [T**3, T**2, T, 1], [0, 0, 1, 0], [3 * T**2, 2 * T, 1, 0]])
-b = np.array([[r0], [target_r], [dr0], [target_dr]])
 
-a, b, c, d = np.linalg.solve(a, b)
-r = a * t**3 + b * t**2 + c * t + d
-ddr = 6 * a * t + 2 * b
+def poly_guidance(t, S, tf):
+    a = np.array(
+        [[0, 0, 0, 1], [tf**3, tf**2, tf, 1], [0, 0, 1, 0], [3 * tf**2, 2 * tf, 1, 0]]
+    )
+    b = S
 
-# --- Polynomial Guidance (theta) ---
-a = np.array([[0, 0, 0, 1], [T**3, T**2, T, 1], [0, 0, 1, 0], [3 * T**2, 2 * T, 1, 0]])
-b = np.array([[theta0], [target_theta], [dtheta0], [target_dtheta]])
+    a, b, c, d = np.linalg.solve(a, b)
 
-a, b, c, d = np.linalg.solve(a, b)
-theta = a * t**3 + b * t**2 + c * t + d
-ddtheta = 6 * a * t + 2 * b
+    f = a * t**3 + b * t**2 + c * t + d
+    df = 3 * a * t**2 + 2 * b * t + c
+    ddf = 6 * a * t + 2 * b
+    return f, df, ddf
 
+
+r, dr, ddr = poly_guidance(t, [r0, target_r, dr0, target_dr], T)
+
+theta, dtheta, ddtheta = poly_guidance(
+    t, [theta0, target_theta, dtheta0, target_dtheta], T
+)
+
+# --- Thrust Curve ---
+F_r_max = []
+F_theta_max = []
+F_max = []
+T = np.arange(550, 650, 10)
+for tf in T:
+    t = np.linspace(0, tf, 100)
+    r, dr, ddr = poly_guidance(t, [r0, target_r, dr0, target_dr], tf)
+    theta, dtheta, ddtheta = poly_guidance(
+        t, [theta0, target_theta, dtheta0, target_dtheta], tf
+    )
+
+    F_r = m0 * (ddr + mu / r**2 - r * dtheta**2)
+    F_theta = m0 * (r * ddtheta - 2 * dr * dtheta)
+    F_r_max.append(max(F_r))
+    F_theta_max.append(max(F_theta))
+    F_max.append(np.sqrt(F_r_max[-1] ** 2 + F_theta_max[-1] ** 2))
+"""
+# --- Calculations ---
+F_r = m0*(ddr + mu/r**2 - r*dtheta**2)
+F_theta = m0*(r*ddtheta - 2*dr*dtheta)
+F_r_max = max(F_r)
+F_theta_max = max(F_theta)
+
+# --- Printing ---
+print(f"Maximum Radial Thrust: {F_r_max:.2f} N")
+print(f"Maximum Angular Thrust: {F_theta_max:.2f} N")
+"""
+# --- Plotting ---
 fig, axs = plt.subplots(2, 2, figsize=(14, 8))
 
 axs[0, 0].plot(theta * 180 / np.pi, r - r_moon)
@@ -50,15 +84,16 @@ axs[0, 0].set_xlabel("Theta (deg)")
 axs[0, 0].set_ylabel("Radius (m)")
 axs[0, 0].set_title("Radius vs. Theta (Descending Orbit)")
 
-axs[1, 0].plot(t, ddr)
-axs[1, 0].set_xlabel("Time (s)")
-axs[1, 0].set_ylabel("ddr (m/s^2)")
-axs[1, 0].set_title("ddr vs. Time")
+axs[1, 1].plot(T, F_max, label="F_r")
+axs[1, 1].set_xlabel("Thrust (N)")
+axs[1, 1].set_ylabel("Force (N)")
+axs[1, 1].legend()
 
-axs[1, 1].plot(t, ddtheta * 180 / np.pi)
-axs[1, 1].set_xlabel("Time (s)")
-axs[1, 1].set_ylabel("ddtheta (deg/s^2)")
-axs[1, 1].set_title("ddtheta vs. Time")
+axs[1, 0].plot(t, F_r, label="F_r")
+axs[1, 0].plot(t, F_theta, label="F_theta")
+axs[1, 0].set_xlabel("Time (s)")
+axs[1, 0].set_ylabel("Force (N)")
+axs[1, 0].legend()
 
 ax1 = axs[0, 1]
 ax2 = ax1.twinx()
